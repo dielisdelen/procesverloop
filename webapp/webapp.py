26455a384c6ba222ba6d3c9d763367c2b5256e74
dielisdelen <dielisdelen@gmail.com>
2024-04-11 10:48:55 +0200

    "Limiter with Feature Flag"

--- webapp/webapp.py
+++ webapp/webapp.py
@@ -13,9 +13,11 @@ from redis import Redis
 import json
 import os
 
-
 # Load environment variables
 load_dotenv()
+
+# Check if Redis Limiter is enabled
+USE_REDIS_LIMITER = os.getenv('USE_REDIS_LIMITER', 'false').lower() == 'true'
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI')
@@ -24,16 +26,28 @@ app.register_blueprint(api_blueprint, url_prefix='/api')
 
 db.init_app(app)
 
-# Redis and Limiter commands
-redis = Redis(host='pvredis-a42qr8.serverless.eun1.cache.amazonaws.com', port=6379, db=0, decode_responses=True, ssl=True)
+if USE_REDIS_LIMITER:
+    from redis import Redis
+    from flask_limiter import Limiter
+    from flask_limiter.util import get_remote_address
+    
+    redis = Redis(host='pvredis-a42qr8.serverless.eun1.cache.amazonaws.com', port=6379, db=0, decode_responses=True, ssl=True)
 
-limiter = Limiter(
-    app=app,
-    key_func=get_remote_address,  # Use the remote address for rate limiting
-    storage_uri="rediss://pvredis-a42qr8.serverless.eun1.cache.amazonaws.com:6379",
-    default_limits=["50 per hour", "5 per minute"]  # Set sensible defaults
-)
+    limiter = Limiter(
+        app=app,
+        key_func=get_remote_address,  # Use the remote address for rate limiting
+        storage_uri="rediss://pvredis-a42qr8.serverless.eun1.cache.amazonaws.com:6379",
+        default_limits=["50 per hour", "5 per minute"]  # Set sensible defaults
+    )
+else:
+    # Define a dummy limiter decorator that does nothing
+    class DummyLimiter:
+        def limit(self, *args, **kwargs):
+            def decorator(f):
+                return f
+            return decorator
 
+    limiter = DummyLimiter()
 
 with app.app_context():
     db.create_all()
