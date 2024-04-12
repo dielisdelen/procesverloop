e1b1dc11099fafb1220b13e4bef2bb7a665a3998
dielisdelen <dielisdelen@gmail.com>
2024-04-12 11:21:53 +0200

    "Scraper Fix v0.6"

--- webapp/CaseExtractor.py
+++ webapp/CaseExtractor.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 import time
 import logging
 
+# Configure logging
 logging.basicConfig(filename='/var/log/scraperlogs/logfile.log',
                     level=logging.INFO,
                     format='%(asctime)s - %(levelname)s - %(message)s')
@@ -10,17 +11,31 @@ logging.basicConfig(filename='/var/log/scraperlogs/logfile.log',
 def scrape_case(ecli_id):
     options = webdriver.ChromeOptions()
     options.add_argument('--no-sandbox')  # Bypass OS security model, REQUIRED on Linux if running as root
-    options.add_argument('--headless=new')  # Run Chrome in headless mode
+    options.add_argument('--headless')  # Run Chrome in headless mode
     options.add_argument('--disable-dev-shm-usage')  # Overcome limited resource problems
-    options.add_argument("--disable-gpu")
+    options.add_argument('--disable-gpu')
     options.add_argument('--remote-debugging-pipe')
-    
-    try:
-        driver = webdriver.Chrome(options=options)
-        logging.info('WebDriver started successfully')
-    except Exception as e:
-        logging.error('Failed to start WebDriver', exc_info=True)
-        return {}, "Failed to initialize the WebDriver."
+    options.add_argument('--user-data-dir=/var/www/chrome-data')  # Custom user data directory
+    options.add_argument('--disk-cache-dir=/var/www/chrome-cache')  # Custom cache directory
+
+    # Ensure the HOME and TMPDIR are set for www-data when initializing the driver
+    with webdriver.Chrome(options=options, service_args=['--verbose', '--log-path=/var/log/scraperlogs/chromedriver.log']) as driver:
+        try:
+            # Build and log the URL
+            url = f"https://uitspraken.rechtspraak.nl/#!/details?id={ecli_id}"
+            logging.info(f'Navigating to URL: {url}')
+            driver.get(url)
+            
+            # Wait and log dynamic content loading
+            time.sleep(6)
+            logging.info('Waiting for page content to load')
+            
+            # Retrieve and log page source
+            html = driver.page_source
+            logging.info('Page source retrieved')
+        except Exception as e:
+            logging.error('Error during page navigation and data retrieval', exc_info=True)
+            return {}, "Error during navigation or data retrieval."
 
     try:
         # Build and log the URL
