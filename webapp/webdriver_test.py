112e9d487bde66d7553f0181cc8c3d470d90968a
dielisdelen <dielisdelen@gmail.com>
2024-04-12 12:57:45 +0200

    'Scraper Fix v0.14"

--- webapp/webdriver_test.py
+++ webapp/webdriver_test.py
@@ -13,13 +13,11 @@ options.add_argument('--disable-dev-shm-usage')  # Overcome limited resource pro
 options.add_argument("--disable-gpu")
 options.add_argument('--remote-debugging-pipe')
 
+driver = None
 try:
     driver = webdriver.Chrome(options=options)
     logging.info('WebDriver started successfully')
-except Exception as e:
-    logging.error('Failed to start WebDriver', exc_info=True)
 
-try:
     # Build and log the URL
     url = f"https://uitspraken.rechtspraak.nl/#!/details?id=ECLI:NL:HR:2005:AT3511"
     logging.info(f'Navigating to URL: {url}')
@@ -29,9 +27,14 @@ try:
     time.sleep(6)
     logging.info('Waiting for page content to load')
         
-     # Retrieve and log page source
+    # Retrieve and log page source
     html = driver.page_source
     logging.info('Page source retrieved')
-except Exception as e:
-    logging.error('Error during page navigation and data retrieval', exc_info=True)
 
+except Exception as e:
+    logging.error('Error during WebDriver operation', exc_info=True)
+
+finally:
+    if driver:
+        driver.quit()
+        logging.info('WebDriver closed')
