74f398bbe5beb1dc81a5a69832a199e5aad6c3fe
dielisdelen <dielisdelen@gmail.com>
2024-04-16 09:37:19 +0200

    "Celery fix v0.12"

---
+++ webapp/minimal_app.py
@@ -0,0 +1,32 @@
+# minimal_app.py
+from flask import Flask
+from celery import Celery
+
+app = Flask(__name__)
+app.config['REDIS_URI'] = 'rediss://pvredis-a42qr8.serverless.eun1.cache.amazonaws.com:6379'
+
+def make_celery(app):
+    redis_uri = app.config['REDIS_URI']
+    celery = Celery(app.import_name, broker=redis_uri, backend=redis_uri)
+
+    class ContextTask(celery.Task):
+        def __call__(self, *args, **kwargs):
+            with app.app_context():
+                return super(ContextTask, self).__call__(*args, **kwargs)
+    
+    celery.Task = ContextTask
+    return celery
+
+celery = make_celery(app)
+
+@celery.task
+def add_together(a, b):
+    return a + b
+
+@app.route('/')
+def hello():
+    result = add_together.delay(23, 42)
+    return f'Result: {result.get(timeout=10)}'
+
+if __name__ == '__main__':
+    app.run(debug=True)
