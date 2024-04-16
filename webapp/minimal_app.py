85ee05b0a36119a13be21eceb75222b099e0d51d
dielisdelen <dielisdelen@gmail.com>
2024-04-16 10:44:52 +0200

    'Celery fix v0.18"

--- webapp/minimal_app.py
+++ webapp/minimal_app.py
@@ -8,19 +8,10 @@ def make_celery(app):
     redis_uri = app.config['REDIS_URI']
     celery = Celery(app.import_name, broker=redis_uri, backend=redis_uri)
 
-    # Additional SSL parameters for secure Redis connection
-    ssl_options = {
-        'ssl_cert_reqs': 'required'  # Change to 'optional' or 'none' as per your Redis setup and security requirements
-    }
-    
-    # Update Redis URL with additional SSL options and configure key prefix with hash tags
-    celery.conf.update(
-        broker_use_ssl=ssl_options,
-        redis_backend_use_ssl=ssl_options,
-        task_default_queue='celery#{my_app}',
-        task_default_exchange='celery#{my_app}',
-        task_default_routing_key='celery#{my_app}',
-    )
+    celery.conf.update({
+        'broker_url': redis_uri,
+        'result_backend': redis_uri
+    })
 
     # Ensure that tasks are executed in the Flask application context
     class ContextTask(celery.Task):
