320a3d9ed4507d22e3f7d512decebb547545cc57
dielisdelen <dielisdelen@gmail.com>
2024-04-18 11:33:20 +0200

    "OpenAI JSON update"

--- webapp/case_extractor_static.py
+++ webapp/case_extractor_static.py
@@ -10,7 +10,7 @@ os.environ['TMPDIR'] = '/var/www/tmp'
 # Configure logging
 if os.getenv('LOGGING_ENABLED', 'false').lower() == 'true':
     logging.basicConfig(filename='/run/procesverloop-logs/logfile.log',
-                        level=logging.INFO,
+                        level=logging.DEBUG,
                         format='%(asctime)s - %(levelname)s - %(message)s')
 else:
     logging.disable(logging.CRITICAL)  # Disable all logging
